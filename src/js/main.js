23e4224be53ed91d613e8dddd18e7d76e97e8bdd
yehor1koval@gmail.com <yehor1koval@gmail.com>
2023-01-02 23:46:24 +0200

    End lesson about create styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,27 @@
 import $ from './lib/lib';
 
-$('button').on('click', function() {
-    $('div').eq(1).toggleClass('active');
-});
+// $('button').on('click', function() {
+//     $('div').eq(1).toggleClass('active');
+// });
 
-$('div').click(function() {
-    console.log($(this).index());
-});
+// $('div').click(function() {
+//     console.log($(this).index());
+// });
 
 // console.log($('div').eq(2).find('.some'));
 // console.log($('.some').closest('.findmeq'));
-$('button').fadeIn(1800);
+// $('button').fadeIn(1800);
 
 // console.log($('button').html('Hello'));
+
+$('#first').on('click', () => {
+    $('div').eq(1).fadeOut(800);
+});
+
+$('[data-count="second"]').on('click', () => {
+    $('div').eq(2).fadeOut(800);
+});
+
+$('button').eq(2).on('click', () => {
+    $('.w-500').fadeOut(800);
+});
