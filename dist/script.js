1a1949840a66487c48134849a39ef1ed16392d65
yehor1koval@gmail.com <yehor1koval@gmail.com>
2023-01-03 21:39:20 +0200

    Add new animation fadeToggle

--- dist/script.js
+++ dist/script.js
@@ -432,6 +432,28 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.fadeOut = function (dura
   }
   return this;
 };
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.fadeToggle = function (duration, display, final) {
+  for (let i = 0; i < this.length; i++) {
+    if (window.getComputedStyle(this[i]).display === 'none') {
+      this[i].style.display = display || 'block';
+      const _fadeIn = complection => {
+        this[i].style.opacity = complection;
+      };
+      const animate = this.animateOverTime(duration, _fadeIn, final);
+      requestAnimationFrame(animate);
+    } else {
+      const _fadeOut = complection => {
+        this[i].style.opacity = 1 - complection;
+        if (complection === 1) {
+          this[i].style.display = 'none';
+        }
+      };
+      const animate = this.animateOverTime(duration, _fadeOut, final);
+      requestAnimationFrame(animate);
+    }
+  }
+  return this;
+};
 
 /***/ }),
 
@@ -504,13 +526,13 @@ __webpack_require__.r(__webpack_exports__);
 // console.log($('button').html('Hello'));
 
 Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('#first').on('click', () => {
-  Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('div').eq(1).fadeOut(800);
+  Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('div').eq(1).fadeToggle(800);
 });
 Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('[data-count="second"]').on('click', () => {
-  Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('div').eq(2).fadeOut(800);
+  Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('div').eq(2).fadeToggle(800);
 });
 Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('button').eq(2).on('click', () => {
-  Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('.w-500').fadeOut(800);
+  Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('.w-500').fadeToggle(800);
 });
 
 /***/ })
--- src/js/lib/modules/effects.js
+++ src/js/lib/modules/effects.js
@@ -59,3 +59,32 @@ $.prototype.fadeOut = function(duration, final) {
 
     return this;
 };
+
+$.prototype.fadeToggle = function(duration, display, final) {
+    for (let i = 0; i < this.length; i++) {
+        if (window.getComputedStyle(this[i]).display === 'none') {
+            this[i].style.display = display || 'block';
+
+            const _fadeIn = (complection) => {
+                this[i].style.opacity = complection;
+            };
+
+            const animate = this.animateOverTime(duration, _fadeIn, final);
+            requestAnimationFrame(animate);
+        } else {
+            const _fadeOut = (complection) => {
+                this[i].style.opacity = 1 - complection;
+    
+                if (complection === 1) {
+                    this[i].style.display = 'none';
+                }
+    
+            };
+    
+            const animate = this.animateOverTime(duration, _fadeOut, final);
+            requestAnimationFrame(animate);
+        }
+    }
+
+    return this;
+};
