0b81e0761bb64d10c328a45238378943eb860cc2
yehor1koval@gmail.com <yehor1koval@gmail.com>
2023-01-06 23:00:01 +0200

    End lesson about accordion

--- src/js/lib/components/tab.js
+++ src/js/lib/components/tab.js
@@ -4,6 +4,7 @@ $.prototype.tab = function() {
     for (let i = 0; i < this.length; i++) {
         $(this[i]).on('click', () => {
             $(this[i])
+                .fadeIn(500)
                 .addClass('tab-item--active')
                 .siblings()
                 .removeClass('tab-item--active')
